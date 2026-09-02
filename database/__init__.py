c97c1e5689a6b30a8d141f382d5117dd2bbb8a88
Aashu Raj <146524948+Aashu-Raj@users.noreply.github.com>
2026-09-02 22:45:56 +0530

    Delete database directory

--- database/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-database/__init__.py
-
-Convenience re-exports for the database package.
-"""
-from database.base import Base
-from database.session import AsyncSessionLocal, engine, get_async_session, get_db
-
-__all__ = ["Base", "engine", "AsyncSessionLocal", "get_async_session", "get_db"]
